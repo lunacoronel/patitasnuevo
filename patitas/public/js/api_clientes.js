f6bfdf5a05e71416ce1dcc884d3413f716a6f6ba
MATIAS AQUINO <138217796+esnait09@users.noreply.github.com>
2024-08-07 20:48:08 -0300

    Add files via upload
    me rindo usamos la manera vieja jaaj

---
+++ patitas/public/js/api_clientes.js
@@ -0,0 +1,38 @@
+
+        document.addEventListener('DOMContentLoaded', function() {
+            const peliculasContainer = document.getElementById('clientes-container');
+
+            function obtenerRazas() {
+                fetch('https://dog.ceo/api/breeds/list/all')
+                    .then(response => response.json())
+                    .then(data => {
+                        const razas = Object.keys(data.message);
+                        razas.forEach(raza => {
+                            obtenerImagenDeRaza(raza);
+                        });
+                    })
+                    .catch(error => {
+                        console.error('Error al obtener las razas:', error);
+                    });
+            }
+
+            function obtenerImagenDeRaza(raza) {
+                fetch(`https://dog.ceo/api/breed/${raza}/images/random`)
+                    .then(response => response.json())
+                    .then(data => {
+                        const razaHTML = document.createElement('div');
+                        razaHTML.classList.add('raza-container');
+                        razaHTML.innerHTML = `
+                            <h3>${raza}</h3>
+                            <img src="${data.message}" alt="Imagen de ${raza}">
+                        `;
+                        peliculasContainer.appendChild(razaHTML);
+                    })
+                    .catch(error => {
+                        console.error(`Error al obtener la imagen de la raza ${raza}:`, error);
+                    });
+            }
+
+            obtenerRazas();
+        });
+  
